8f5efe97358b819a7bbef598a71ad19c1a7d3a9a
Md Shahedul Islam Khan <66883156+KhanShahed1NWPU@users.noreply.github.com>
2021-10-09 23:38:49 +0600

    Add files via upload

---
+++ src/com/shahed_amirbek/Launcher.java
@@ -0,0 +1,48 @@
+package com.shahed_amirbek;
+
+import com.shahed_amirbek.controller.persistence.PersistenceAccess;
+import com.shahed_amirbek.controller.persistence.ValidAccount;
+import com.shahed_amirbek.controller.services.LoginService;
+import com.shahed_amirbek.model.EmailAccount;
+import com.shahed_amirbek.view.ViewFactory;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if(validAccountList.size() > 0) {
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount: validAccountList){
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
+    }
+
+
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount: emailManager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
+    }
+}
---
+++ src/com/shahed_amirbek/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.shahed_amirbek.controller;
+
+import com.shahed_amirbek.EmailManager;
+import com.shahed_amirbek.view.ViewFactory;
+
+public abstract class BaseController {
+
+    protected EmailManager emailManager;
+    protected ViewFactory viewFactory;
+    private String fxmlName;
+
+    public BaseController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        this.emailManager = emailManager;
+        this.viewFactory = viewFactory;
+        this.fxmlName = fxmlName;
+    }
+
+    public String getFxmlName() {
+        return fxmlName;
+    }
+}
